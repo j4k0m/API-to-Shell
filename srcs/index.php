2d07a30409193c366d78b23f8db14bedd35b0e69
Jakom <ael-hask@student.1337.ma>
2021-08-30 18:38:06 +0100

    target source files.

---
+++ srcs/index.php
@@ -0,0 +1,15 @@
+<?php
+ /*
+  *  * GET  -> doc
+  *   * POST -> action
+  *    *  methods:
+  *     *    - /login      
+  *                   (user,password)       -> token
+  *                    *    - /register                 (user,password)       -> token
+  *                     *    - /files                    (token)               -> files
+  *                      *    - /file                     (token, uuid, sig)    -> file
+  *                       *    - /upload                   (token, file)         -> ok 
+  *                        */
+require('router.php');
+
+?> 
